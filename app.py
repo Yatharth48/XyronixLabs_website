66b6665aa4e4c6377e5aa7fafd3aac63abb45f75
Aditya seth <adityaseth936@gmail.com>
2025-01-31 23:53:14 +0530

    Delete app.py
    Signed-off-by: Aditya seth <adityaseth936@gmail.com>

--- app.py
+++
@@ -1,9 +0,0 @@
-from utils.logger import logger
-
-# Example usage
-logger.log('User logged in', 'auth')
-logger.log('User requested resource', 'request')
-
-# Retrieve logs by category
-auth_logs = logger.get_logs_by_category('auth')
-print('Auth Logs:', [log.dict() for log in auth_logs])
