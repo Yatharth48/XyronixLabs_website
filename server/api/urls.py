efbdcdcb31b5b9ec72f57c4fd87993df7f4fc496
Aditya seth <adityaseth936@gmail.com>
2025-02-07 08:10:18 +0530

    created url, views for all the pages like Home About Us Products Services Research Gallery Contact Us SignIn Signup

--- server/api/urls.py
+++ server/api/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     path('sample/', views.sample_api),
+    path('home/', views.home, name='home'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('products/', views.products, name='products'),
+    path('services/', views.services, name='services'),
+    path('research/', views.research, name='research'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('contact-us/', views.contact_us, name='contact_us'),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,4 +1,3 @@
-# filepath: /c:/Users/adity/OneDrive/Desktop/work/web6.0/server/api/views.py
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import SampleSerializer
@@ -10,3 +9,39 @@ def sample_api(request):
     }
     serializer = SampleSerializer(data)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def home(request):
+    return Response({"message": "Welcome to the Home page"})
+
+@api_view(['GET'])
+def about_us(request):
+    return Response({"message": "About Us page"})
+
+@api_view(['GET'])
+def products(request):
+    return Response({"message": "Products page"})
+
+@api_view(['GET'])
+def services(request):
+    return Response({"message": "Services page"})
+
+@api_view(['GET'])
+def research(request):
+    return Response({"message": "Research page"})
+
+@api_view(['GET'])
+def gallery(request):
+    return Response({"message": "Gallery page"})
+
+@api_view(['GET'])
+def contact_us(request):
+    return Response({"message": "Contact Us page"})
+
+@api_view(['GET'])
+def signin(request):
+    return Response({"message": "Sign In page"})
+
+@api_view(['GET'])
+def signup(request):
+    return Response({"message": "Sign Up page"})
