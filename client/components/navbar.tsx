2f3ba487fae90cef481a37f5d0e7851907a646e9
Aditya seth <adityaseth936@gmail.com>
2025-08-04 03:59:57 +0530

    Update navbar.tsx
    Signed-off-by: Aditya seth <adityaseth936@gmail.com>

--- client/components/navbar.tsx
+++ client/components/navbar.tsx
@@ -48,9 +48,9 @@ export default function Navbar() {
           <Link href="/gallery" className="text-sm hover:text-purple-400 transition-colors">
             Gallery
           </Link>
-          <Button asChild variant="outline" className="bg-[#9fef00] text-black">
-            <Link href="/signin">Sign In</Link>
-          </Button>
+{/*           <Button asChild variant="outline" className="bg-[#9fef00] text-black"> */}
+{/*             <Link href="/signin">Sign In</Link> */}
+          // </Button>
         </nav>
 
         {/* Mobile Menu */}
@@ -84,9 +84,9 @@ export default function Navbar() {
               >
                 Gallery
               </Link>
-              <Button asChild variant="outline" className="w-full bg-[#9fef00]" onClick={() => setIsOpen(false)}>
-                <Link href="/signin">Sign In</Link>
-              </Button>
+{/*               <Button asChild variant="outline" className="w-full bg-[#9fef00]" onClick={() => setIsOpen(false)}> */}
+{/*                 <Link href="/signin">Sign In</Link> */}
+              // </Button>
             </nav>
           </SheetContent>
         </Sheet>
