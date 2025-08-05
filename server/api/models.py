d80e0666b04a8ed80ca0d9a7f6ae7cf9f2039edd
Aditya seth <adityaseth936@gmail.com>
2025-08-05 23:08:20 +0530

    Delete server directory
    Signed-off-by: Aditya seth <adityaseth936@gmail.com>

--- server/api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class SampleModel(models.Model):
-    message = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.message
--- server/api/serializers.py
+++
@@ -1,5 +0,0 @@
-# filepath: /c:/Users/adity/OneDrive/Desktop/work/web6.0/server/api/serializers.py
-from rest_framework import serializers
-
-class SampleSerializer(serializers.Serializer):
-    message = serializers.CharField(max_length=200)
